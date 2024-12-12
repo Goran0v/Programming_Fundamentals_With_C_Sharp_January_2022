813b48b2eb07653fe1f524a6e4bd507ab16bfe0a
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-12-12 21:14:07 +0200

    Added the exercises

---
+++ RegularExpressions/MatchPhoneNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+            string names = Console.ReadLine();
+            MatchCollection matched = Regex.Matches(names, regex);
+
+            string[] matches = Regex.Matches(names, regex).Select(x => x.Value).ToArray();
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
---
+++ RegularExpressions/RegularExpressions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b([A-Z][a-z]{1,}) ([A-Z][a-z]{1,})\b";
+            string names = Console.ReadLine();
+            MatchCollection matched = Regex.Matches(names, regex);
+
+            foreach (var match in matched)
+            {
+                Console.Write($"{match} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
