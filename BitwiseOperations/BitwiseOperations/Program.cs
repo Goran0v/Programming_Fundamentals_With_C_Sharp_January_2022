34d3cb6c57fcee309d91a704ee8ee70fbda6da03
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-12-12 21:16:30 +0200

    Added the exercise

---
+++ BitwiseOperations/BitwiseOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BitwiseOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int counterForZero = 0;
+            int counterForOne = 0;
+            while (n != 0)
+            {
+                if (n % 2 == 0)
+                {
+                    counterForZero++;
+                }
+                else if (n % 2 == 1)
+                {
+                    counterForOne++;
+                }
+                n /= 10;
+            }
+
+            if (b == 0)
+            {
+                Console.WriteLine(counterForZero);
+            }
+            else
+            {
+                Console.WriteLine(counterForOne);
+            }
+        }
+    }
+}
